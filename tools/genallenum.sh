47e4f75101f1833b36daff00fefd7b1eb52129d8
Tuomas Jorma Juhani Räsänen <tuomasjjrasanen@tjjr.fi>
2011-10-22 14:50:22 +0300

    Tools for generating ev.py not needed: functionality is now part of setup.py

--- tools/genallenum.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -eu
-
-python genenum.py 0x01 KEY > ../src/ev.py
-python genenum.py 0x01 BTN >> ../src/ev.py
-python genenum.py 0x02 REL >> ../src/ev.py
-python genenum.py 0x03 ABS >> ../src/ev.py
