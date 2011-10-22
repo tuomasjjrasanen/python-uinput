f10e5b8a21f61439caebb5f74f4108e41ea77927
Tuomas Jorma Juhani Räsänen <tuomasjjrasanen@tjjr.fi>
2011-10-22 14:50:22 +0300

    Tools for generating ev.py not needed: functionality is now part of setup.py

--- tools/genenum.py
+++
@@ -1,9 +0,0 @@
-import re
-import sys
-
-if __name__ == "__main__":
-    with open("/usr/include/linux/input.h") as f:
-        for line in f:
-            match = re.match(r"^#define (" + sys.argv[2] + "_.*)\t+((?:0x[0-9a-f]+)|(?:\d+))", line)
-            if match:
-                print "%s = (%s, %s)" % (match.group(1).strip(), sys.argv[1], match.group(2).strip())
