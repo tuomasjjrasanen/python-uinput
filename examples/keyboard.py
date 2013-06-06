ac8602e8b8077e434ae410c7dc8c54af10bfb76a
Tuomas Räsänen <tuomasjjrasanen@tjjr.fi>
2013-06-06 21:28:17 +0300

    Update examples to use Device.emit_click()

--- examples/keyboard.py
+++ examples/keyboard.py
@@ -10,16 +10,11 @@ def main():
 
     device = uinput.Device(events)
 
-    device.emit(uinput.KEY_H, 1) # Press.
-    device.emit(uinput.KEY_H, 0) # Release.
-    device.emit(uinput.KEY_E, 1)
-    device.emit(uinput.KEY_E, 0)
-    device.emit(uinput.KEY_L, 1)
-    device.emit(uinput.KEY_L, 0)
-    device.emit(uinput.KEY_L, 1)
-    device.emit(uinput.KEY_L, 0)
-    device.emit(uinput.KEY_O, 1)
-    device.emit(uinput.KEY_O, 0)
+    device.emit_click(uinput.KEY_H)
+    device.emit_click(uinput.KEY_E)
+    device.emit_click(uinput.KEY_L)
+    device.emit_click(uinput.KEY_L)
+    device.emit_click(uinput.KEY_O)
 
 if __name__ == "__main__":
     main()
