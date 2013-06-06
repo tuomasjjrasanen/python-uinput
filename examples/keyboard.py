1020cd204445a167dc934d45307ac3669b08de7f
Tuomas Räsänen <tuomasjjrasanen@tjjr.fi>
2013-06-06 22:44:08 +0300

    Add short magical sleep to keyboard example

--- examples/keyboard.py
+++ examples/keyboard.py
@@ -1,3 +1,5 @@
+import time
+
 import uinput
 
 def main():
@@ -9,6 +11,16 @@ def main():
         )
 
     device = uinput.Device(events)
+    time.sleep(1) # This is required here only for demonstration
+                  # purposes. Without this, the underlying machinery
+                  # might not have time to assign a proper handler for
+                  # our device before the execution of this script
+                  # reaches the end and the device is destroyed. At
+                  # least this seems to be the case with X11 and its
+                  # generic event device handlers. Without this magical
+                  # sleep, "hello" might not get printed because this
+                  # example exits before X11 gets its handlers ready for
+                  # processing events from this device.
 
     device.emit_click(uinput.KEY_H)
     device.emit_click(uinput.KEY_E)
