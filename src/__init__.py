2a685540aac1954fc2bfd9b5101d7c456862c124
Tuomas Räsänen <tuomasjjrasanen@tjjr.fi>
2013-06-06 22:26:25 +0300

    Add function chars_to_events()

--- src/__init__.py
+++ src/__init__.py
@@ -76,6 +76,58 @@ _libsuinput.suinput_emit.errcheck = _error_handler
 _libsuinput.suinput_syn.errcheck = _error_handler
 _libsuinput.suinput_destroy.errcheck = _error_handler
 
+CHAR_MAP = {
+    "a":  KEY_A,
+    "b":  KEY_B,
+    "c":  KEY_C,
+    "d":  KEY_D,
+    "e":  KEY_E,
+    "f":  KEY_F,
+    "g":  KEY_G,
+    "h":  KEY_H,
+    "i":  KEY_I,
+    "j":  KEY_J,
+    "k":  KEY_K,
+    "l":  KEY_L,
+    "m":  KEY_M,
+    "n":  KEY_N,
+    "o":  KEY_O,
+    "p":  KEY_P,
+    "q":  KEY_Q,
+    "r":  KEY_R,
+    "s":  KEY_S,
+    "t":  KEY_T,
+    "u":  KEY_U,
+    "v":  KEY_V,
+    "w":  KEY_W,
+    "x":  KEY_X,
+    "y":  KEY_Y,
+    "z":  KEY_Z,
+    "1":  KEY_1,
+    "2":  KEY_2,
+    "3":  KEY_3,
+    "4":  KEY_4,
+    "5":  KEY_5,
+    "6":  KEY_6,
+    "7":  KEY_7,
+    "8":  KEY_8,
+    "9":  KEY_9,
+    "0":  KEY_0,
+    "\t": KEY_TAB,
+    "\n": KEY_ENTER,
+    " ":  KEY_SPACE,
+    ".":  KEY_DOT,
+    ",":  KEY_COMMA,
+    "/":  KEY_SLASH,
+    "\\": KEY_BACKSLASH,
+    }
+
+def chars_to_events(chars):
+    events = []
+    for char in chars:
+        events.append(CHAR_MAP.get(char))
+    return events
+
 class Device(object):
 
     """Device handle.
