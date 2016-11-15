83ea432979d530d7d5440ed7b37fc76adebe2cc9
devbharat <bharat.tak8@gmail.com>
2016-11-15 18:28:54 +0100

    Added switches

--- examples/joystick.py
+++ examples/joystick.py
@@ -152,9 +152,96 @@ class stick_state(object):
             self.release_stick()
             self.emit(device)
 
+class switch_state(object):
+    def __init__(self, name, switch, key, spring_back=True):
+        self.name = name                            # The name of the button
+        self.switch = switch                        # The switch on the joystick that this switch maps to
+        self.key = key                              # The key on the keyboard that maps to this switch
+        self.spring_back = spring_back              # Does the switch switch back to of-statee on release?
+        self.active = False                         # True if key is held pressed
+        self.active_previous = False                # Holds the previous active value
+        self.display_ready = False                  # Whether optional display params have been set
+        self.display_height = 0                     # Height on the display screen
+        self.display_width = 0                      # Width on the display screen
+        self.display_hor = True                     # Whether the display bar is horizontal, else vertical
+        self.display_bar_g = []
+        self.display_bar_b = []
+
+    def keypress(self):
+        self.active_previous = self.active
+        if self.spring_back:
+            self.active = True                      # self.active toggles automatically on key release
+        else:
+            self.active = not self.active           # toggle self.active on keypress
+
+    def release_switch(self):
+        if not self.spring_back:
+            pass
+        else:
+            self.active_previous = self.active
+            self.active = False
+
+    def emit(self, device):
+        # emit effeciently
+        if self.active_previous != self.active:
+            device.emit(self.switch, int(self.active))
+            self.active_previous = self.active
+            if self.display_ready:
+                self.display()
+
+    def set_display(self, offset_height, offset_width, horizontal):
+        self.display_height = offset_height
+        self.display_width = offset_width
+        self.display_hor = horizontal
+        if horizontal:
+            filename = os.path.join(dir, 'media/hg.png')
+            self.display_bar_g = pygame.image.load(filename)
+            filename = os.path.join(dir, 'media/hb.png')
+            self.display_bar_b = pygame.image.load(filename)
+        else:
+            filename = os.path.join(dir, 'media/vg.png')
+            self.display_bar_g = pygame.image.load(filename)
+            filename = os.path.join(dir, 'media/vb.png')
+            self.display_bar_b = pygame.image.load(filename)
+        self.display_ready = True
+
+    def display(self):
+        if not self.display_ready:
+            pass
+        else:
+            # Fill the entire bar
+            for i in range(256):
+                if i <= self.emit_val:
+                    # Fill green
+                    if self.display_hor:
+                        windowSurface.blit(self.display_bar_g,(self.display_width + i, self.display_height))
+                    else:
+                        windowSurface.blit(self.display_bar_g,(self.display_width, self.display_height - i))
+                else:
+                    # Fill grey
+                    if self.display_hor:
+                        windowSurface.blit(self.display_bar_b,(self.display_width + i, self.display_height))
+                    else:
+                        windowSurface.blit(self.display_bar_b,(self.display_width, self.display_height - i))
+            # Render it
+            pygame.display.flip()
+
+    def update_event(self, event, device):
+        if event.type == KEYUP:
+            if (event.key == self.key):
+                self.release_switch()
+                self.emit(device)
+        elif event.type == KEYDOWN:
+            if (event.key == self.key):
+                self.keypress()
+                self.emit(device)
+
 def main():
     events = (
-        uinput.BTN_JOYSTICK,
+        uinput.BTN_TRIGGER,
+        uinput.BTN_A,
+        uinput.BTN_B,
+        uinput.BTN_C,
         uinput.ABS_X + (0, 255, 0, 0),
         uinput.ABS_Y + (0, 255, 0, 0),
         uinput.ABS_THROTTLE + (0, 255, 0, 0),
@@ -162,6 +249,7 @@ def main():
         )
 
     sticks = []
+    switches = []
 
     # create sticks
     roll_stick = stick_state('Roll', uinput.ABS_X, K_RIGHT, K_LEFT)
@@ -177,15 +265,28 @@ def main():
     rud_stick.set_display(360, 39, True)
     sticks.append(rud_stick)
 
+    # create switches
+    switch_1 = switch_state('1', uinput.BTN_TRIGGER, K_q)
+    switches.append(switch_1)
+    switch_2 = switch_state('2', uinput.BTN_A, K_1, False)
+    switches.append(switch_2)
+    switch_3 = switch_state('3', uinput.BTN_B, K_2, False)
+    switches.append(switch_3)
+    switch_4 = switch_state('4', uinput.BTN_C, K_3, False)
+    switches.append(switch_4)
+
     with uinput.Device(events) as device:
         while True:
             # event handling loop
             for event in pygame.event.get():
                 for stick in sticks:
                     stick.update_event(event)
+                for switch in switches:
+                    switch.update_event(event, device)
             for stick in sticks:
                     stick.update_stick(device)
             time.sleep(0.0005)
+            #device.emit_click(uinput.BTN_JOYSTICK)
 
 if __name__ == "__main__":
     main()
